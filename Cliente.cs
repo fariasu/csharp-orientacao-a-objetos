180c831d8224df2adbec6b304ac63588d9597110
Farias <pablogarciafarias@gmail.com>
2022-08-13 17:44:47 -0300

    Initial commit

---
+++ Cliente.cs
@@ -0,0 +1,10 @@
+﻿namespace bytebank.Titular
+{
+    public class Cliente
+    {
+
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Profissao { get; set; }
+    }
+}
---
+++ ContaCorrente.cs
@@ -0,0 +1,90 @@
+﻿using bytebank.Titular;
+
+namespace bytebank
+{
+    public class ContaCorrente
+    {
+
+        public Cliente Titular { get; set; }
+        public string Nome_Agencia { get; set; }
+        public int Numero_agencia
+        {
+            get;
+
+            set;
+            
+
+        }
+        public string Conta
+        {
+            get;
+            set;
+        }
+
+        private double saldo;
+        public double Saldo
+        {
+            get
+            {
+                return saldo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+                else
+                {
+                    saldo = value;
+                }
+            }
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (saldo < valor)
+            {
+                return false;
+            }
+            if (valor < 0)
+            {
+                return false;
+            }
+            else
+            {
+                saldo = saldo - valor;
+                return true;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            if (valor < 0)
+            {
+                return;
+            }
+            saldo = saldo + valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente destino)
+        {
+            if (saldo < valor)
+            {
+                return false;
+            }
+            if (valor < 0)
+            {
+                return false;
+            }
+            else
+            {
+                saldo = saldo - valor;
+                destino.saldo = destino.saldo + valor;
+                return true;
+            }
+        }
+
+       
+    }
+}
